78612eaf70ddf394e84ce3adf36dc98c44418f02
no <no@no.no>
2020-12-17 13:54:40 +0200

    yes

---
+++ btd6mod/Main.cs
@@ -0,0 +1,29 @@
+﻿using MelonLoader;
+using Harmony;
+using NKHook6.Api;
+using NKHook6;
+using Assets.Scripts.Simulation.Towers.Weapons;
+using Assets.Scripts.Unity.UI_New.InGame;
+using NKHook6.Api.Extensions;
+namespace btd6mod
+{
+	public class Main : MelonMod
+	{
+		public override void OnApplicationStart()
+		{
+			base.OnApplicationStart();
+			Logger.Log("btd6mod has loaded!");
+		}
+	}
+
+	[HarmonyPatch(typeof(Weapon), "Initialise")]
+	public class WeaponInitialise_Patch
+	{
+		[HarmonyPostfix]
+		public static void Postfix(Weapon __instance)
+		{
+            InGame.instance.addCash(9999999);
+			InGame.instance.addHealth(9999999);
+		}
+	}
+}
